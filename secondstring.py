79911bcaa9bc1c47c3d1f7aa9f7d4c097362f165
Fezeka Nzimande <60849112+fezeka-tech@users.noreply.github.com>
2021-04-17 20:37:19 +0100

    Update secondstring.py
    fixed spacing

--- secondstring.py
+++ secondstring.py
@@ -1,6 +1,6 @@
 #WEEK 03#
-
 #This is a python function that outputs every second letter in reverse order#
+
 def second_string ( ) :
     sentence = input ("Please enter a sentence: ") .split ("The quick brown fox jumps over the lazy dog. ") #split sentence by space#
     second_letter = [ ] #container to store every second letter#
@@ -9,5 +9,6 @@ def second_string ( ) :
         second_letter.append(words[1::2]) #store every second letter in a word#
 
 strings = list (map(lambda x: x[::-1], second_letter)) #reverse second letter of a word#
+
 print ("The quick brown fox jumps over the lazy dog".join (strings[ ::-1])) #print all in reverse order#
 
