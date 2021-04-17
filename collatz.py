644396f20d95c70b21c7d5bf270ade3d5b53788a
Fezeka Nzimande <60849112+fezeka-tech@users.noreply.github.com>
2021-04-17 20:49:17 +0100

    Update collatz.py
    comment and referencing

--- collatz.py
+++ collatz.py
@@ -1,11 +1,11 @@
-#This is a python function that asks the user to input any positive integer and outputs the successive values of the following calculation#
+#This is a python function that asks the user to input any positive integer and outputs the successive values of the following calculation...#
 def collatz|() :
 
     positive_integer = int(input("Please enter a positive integer: "))
     answer_list = [positive_integer]
     calc = 0
 
-    while positive_integer > 1:
+    while positive_integer > 1:                         #...with the use of while loop and if/else statement#
         if positive_integer % 2 == 0:
             calc = positive_integer / 2
             answer_list.append(int(calc))
@@ -18,5 +18,4 @@ def collatz|() :
     answer = " ".join(list(map(str, answer_list)))
     print(answer)
 
-
-if
+# Collatz Sequence, Stack Overflow; https://stackoverflow.com/questions/33324432/collatz-sequence-python-3
