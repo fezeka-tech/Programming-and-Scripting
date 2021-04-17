07e8a06cdc6cba09be10f29d052f718721e927e7
Fezeka Nzimande <60849112+fezeka-tech@users.noreply.github.com>
2021-04-17 20:34:55 +0100

    Update squareroot.py
    Added reference

--- squareroot.py
+++ squareroot.py
@@ -3,15 +3,21 @@
 def sqrt(x):
     def sqrt_iter(guess):             #variable#
         return guess if good_enough(guess) else sqrt_iter(improve(guess))
+    
     def good_enough(guess):
         tolerance = 1e-3        # this will limit the margin for errors and provide the most accurate value#
         return abs(guess**2 - x) < tolerance
+    
     def improve(guess):
         return (guess + x/guess)/2
     initial_guess = 1.0
+    
     return sqrt_iter(initial_guess) 
 
 
 positiveNumber = input ("Enter a positive number or enter/return to quit: ")
 
 print(sqrt(positiveNumber))
+
+
+# Square roots by Newtown's Method, Mathematics; https://math.stackexchange.com/questions/3524205/square-roots-by-newton-s-method #
