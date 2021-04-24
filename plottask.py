87e1c8bef6a7edadb6d840a5263b6290a63236bc
Fezeka Nzimande <60849112+fezeka-tech@users.noreply.github.com>
2021-04-24 21:24:31 +0100

    Update and rename girlname.py to plottask.py
    Late commit of final weekly task

---
+++ plottask.py
@@ -0,0 +1,17 @@
+# This is a program that displays a plot of the functions f(x)=x, g(x)=x² , and h(x)=x^3 in the range [0,4] on one set of axes #
+
+import numpy as np
+import matplotlib.pyplot as mpt
+
+xpoints = np.array(range(0,4))
+
+mpt.plot(xpoints)
+mpt.show
+
+
+
+
+
+
+
+ 
