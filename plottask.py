24230a70e029f5b8e69d1baee1e076622810385c
Fezeka Nzimande <60849112+fezeka-tech@users.noreply.github.com>
2021-04-25 20:25:36 +0100

    Update plottask.py
    Added code for xsquared and cubed points, and assigned colours

--- plottask.py
+++ plottask.py
@@ -1,12 +1,17 @@
 # This is a program that displays a plot of the functions f(x)=x, g(x)=x² , and h(x)=x^3 in the range [0,4] on one set of axes #
 
 import numpy as np
-import matplotlib.pyplot as mpt
+import matplotlib.pyplot as plt
 
 xpoints = np.array(range(0,4))
 
-mpt.plot(xpoints)
-mpt.show
+plt.plot(xpoints, colour= "orange")
+plt.plot(xpoints, label= "xsquared", colour= "green")
+plt.plot(xpoints, label= "xcubed", colour= "yellow")
+
+plt.legend()
+
+plt.show()
 
 
 
